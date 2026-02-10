757ddb21930f1aacae80c2ca9f4e58eb51d56185
tanakorntangsuk <taengsuk.t@gmail.com>
2026-02-10 15:39:29 +0700

    common kafka for email

---
+++ src/main/java/com/example/common/EmailRequest.java
@@ -0,0 +1,18 @@
+package com.example.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailRequest {
+
+    private String to;
+
+    private String subject;
+
+    private String content;
+
+}
